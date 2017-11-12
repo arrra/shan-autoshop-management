07c1e8d9e3f88ecba328c95d1b5ffa5a4eee4e0c
Sagar Singh <ssing128@gmail.com>
2017-11-11 17:02:33 -0800

    Basic customer model

---
+++ models/Customer.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+  firstName: {type: String},
+  lastName: {type: String},
+  phoneNumber: {type: String},
+  email: {type: String, required: true, unique: true},
+  city: {type: String},
+  state: {type: String},
+  address: {type: String},
+})
+
+CustomerSchema.statics.createCustomer = function(customerAttr, cb) {
+  if(typeof customerAttr !== 'object') {
+    customerAttr = {};
+  };
+
+  if(typeof cb !== 'function'){
+    cb = function(){};
+  };
+
+  const customer = new this(customerAttr);
+
+  customer.save((err) => {
+    if(err) return cb(err);
+
+    cb(null,customer);
+  })
+}
+
+const Customer = mongoose.model('Customer', CustomerSchema);
+
+module.exports = Customer;
