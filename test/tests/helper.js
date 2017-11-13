b8b35c9f99e613a2d3935d54edc065400eedee05
Sagar Singh <ssing128@gmail.com>
2017-11-13 02:27:22 -0800

    Start test suite

---
+++ test/tests/helper.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const db = require('../../db');
+const clearDB = require('mocha-mongoose')(db.uri);
+const chai = require('chai');
+const highlight = require('chai-highlight');
+
+chai.use(highlight);
+global.expect = chai.expect;
+global.assert = chai.assert;
+
+beforeEach((done) => {
+  if(mongoose.connection.db) return done(); 
+  mongoose.connect(db.uri,  (err)=> {
+    if(err) return done(err);
+    clearDB(done);
+  })
+})
+
+afterEach((done) => {
+  mongoose.disconnect(done);
+})
