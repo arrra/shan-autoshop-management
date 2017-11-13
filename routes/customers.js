410b2a525c156dad14adf542608866af3687a388
Sagar Singh <ssing128@gmail.com>
2017-11-13 02:58:19 -0800

    POST /customers create new customer

---
+++ routes/customers.js
@@ -0,0 +1,26 @@
+const Router = require('express').Router;
+const Customer = require('../models/Customer');
+
+const router = Router();
+
+router.post('/', (req, res) => {
+  const customerAttr = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    city: req.body.city,
+    state: req.body.state,
+    address: req.body.address,
+  }
+
+  Customer.createCustomer(customerAttr, (err, customer)=>{
+    if(err){
+      res.status(400).json(err);
+      return err;
+    }
+
+    res.status(201).json(customer);
+  })
+})
+module.exports = router;
